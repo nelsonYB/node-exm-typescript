9a681a1b44e228a014eee89b363d7bec75107530
Nelson <nel.ubeda@gmail.com>
2021-11-10 13:32:44 -0600

    update security

--- controllers/usuarios.ts
+++ controllers/usuarios.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
 
-export const getUsers = (req: Request, res: Response) => {
-    res.json({
-        msg: 'getUsers'
-    })
+export const getUsers = async(req: Request, res: Response) => {
+
+    const users = await User.findAll();
+
+    res.json(users);
 }
 
 export const getUser = (req: Request, res: Response) => {
--- db/connection.ts
+++ db/connection.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize('node', 'root', '', {
+dotenv.config();
+
+const db_user: string = process.env.DB_USERNAME || 'user';
+const db_password: string = process.env.DB_PASSWORD ?? "";
+
+const db = new Sequelize('node', db_user, db_password, {
     host: 'localhost',
     dialect: 'mysql',
     // logging: false
